78b6f99ac8d497cb3f9ab32c51e6da256ae88d05
Danilo Silva <danilo.silva@msn.com>
2022-06-26 02:36:13 -0300

    Handling Bad Requests

--- ASP.NET Core Fundamentals/OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
+++ ASP.NET Core Fundamentals/OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -15,9 +16,16 @@ namespace OdeToFood.Pages.Restaurants
             this.restaurantData = restaurantData;
         }
 
-        public void OnGet(int restaurantId)
+        public IActionResult OnGet(int restaurantId)
         {
             Restaurant = restaurantData.GetById(restaurantId);
+
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
         }
     }
 }
