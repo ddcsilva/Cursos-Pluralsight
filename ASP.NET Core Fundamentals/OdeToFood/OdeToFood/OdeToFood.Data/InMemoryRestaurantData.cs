16cd404501b5449ea76d8571cdc05dd05056aab2
Danilo Silva <danilo.silva@msn.com>
2022-06-25 23:48:03 -0300

    Buiding a Data Access Service

---
+++ ASP.NET Core Fundamentals/OdeToFood/OdeToFood/OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;.
+using System.Linq;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.Italian},
+                new Restaurant { Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Mexican}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
