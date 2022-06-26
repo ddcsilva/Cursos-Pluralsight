71bcf6ea1e080017023c2fef0cd26639449fa03d
Danilo Silva <danilo.silva@msn.com>
2022-06-26 01:55:28 -0300

    Building Detail Page

---
+++ ASP.NET Core Fundamentals/OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class DetailModel : PageModel
+    {
+        public Restaurant Restaurant { get; set; }
+
+        public void OnGet()
+        {
+            Restaurant = new Restaurant();
+        }
+    }
+}
