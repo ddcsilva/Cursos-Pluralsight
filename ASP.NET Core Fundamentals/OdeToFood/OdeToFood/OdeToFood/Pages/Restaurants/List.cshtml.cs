cf40dbf7cb4c5e5e348ee41206c516317e64916d
Danilo Silva <danilo.silva@msn.com>
2022-06-25 23:25:47 -0300

    Using Configuration

--- ASP.NET Core Fundamentals/OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ ASP.NET Core Fundamentals/OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,16 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration config;
+
         public string Message { get; set; }
+        public string MessageConfiguration { get; set; }
+
+        public ListModel(IConfiguration config)
+        {
+            this.config = config;
+        }
 
         public void OnGet()
         {
             Message = "Hello World";
+            MessageConfiguration = config["Message"];
         }
     }
 }
