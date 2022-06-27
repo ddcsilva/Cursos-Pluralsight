1b55b9b5b47fbd7bdfad52c777907834216c72b5
Danilo Silva <danilo.silva@msn.com>
2022-06-26 21:55:57 -0300

    Building a Create  Restaurant Page

--- ASP.NET Core Fundamentals/OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ ASP.NET Core Fundamentals/OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -22,10 +22,17 @@ namespace OdeToFood.Pages.Restaurants
             this.htmlHelper = htmlHelper;
         }
 
-        public IActionResult OnGet(int restaurantId)
+        public IActionResult OnGet(int? restaurantId)
         {
             Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
-            Restaurant = restaurantData.GetById(restaurantId);
+            if (restaurantId.HasValue)
+            {
+                Restaurant = restaurantData.GetById(restaurantId.Value);
+            }
+            else
+            {
+                Restaurant = new Restaurant();
+            }
 
             if (Restaurant == null)
             {
