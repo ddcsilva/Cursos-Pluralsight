77f50341e674425cc02cfa4c1409cd7df49063d9
Danilo Silva <danilo.silva@msn.com>
2022-07-02 21:17:29 -0300

    Creating the Shopping Cart

--- Building Web Applications with ASP.NET Core 3 and 5 MVC/BethanysPieShop/BethanysPieShop/Startup.cs
+++ Building Web Applications with ASP.NET Core 3 and 5 MVC/BethanysPieShop/BethanysPieShop/Startup.cs
@@ -29,6 +29,10 @@ namespace BethanysPieShop
 
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+
+            services.AddHttpContextAccessor();
+            services.AddSession();
             services.AddControllersWithViews();
         }
 
@@ -42,6 +46,7 @@ namespace BethanysPieShop
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
 
             app.UseRouting();
 
