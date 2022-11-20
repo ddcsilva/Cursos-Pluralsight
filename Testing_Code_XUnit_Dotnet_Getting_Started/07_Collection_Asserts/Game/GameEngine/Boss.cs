002e7338a55a40a9809875ee3df3b75b702660db
Danilo Silva <danilo.silva@msn.com>
2022-11-20 11:09:14 -0300

    Assert com Collections

---
+++ Testing_Code_XUnit_Dotnet_Getting_Started/07_Collection_Asserts/Game/GameEngine/Boss.cs
@@ -0,0 +1,8 @@
+﻿namespace GameEngine
+{
+    public class Boss : Inimigo
+    {
+        public override double PoderEspecialTotal => 1000;
+        public override double PoderEspecialUsado => 6;
+    }
+}
