7e7dad741ff29c2a036cfbb0c31333bd6a2893ba
Danilo Silva <danilo.silva@msn.com>
2022-11-20 23:10:16 -0300

    Compartilhando contexto entre classes de teste

---
+++ Testing_Code_XUnit_Dotnet_Getting_Started/17_Sharing_Context_Across_Tests_Classes/Game/GameEngine.Tests/BossShould.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class BossShould
+    {
+        [Fact]
+        public void HaveCorrectPower()
+        {
+            Boss sut = new Boss();
+
+            Assert.Equal(166.667, sut.AtaquePoderEspecialTotal, 3);
+        }
+    }
+}
