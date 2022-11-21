5a5e01794c476852728a6084dcb8887093302eac
Danilo Silva <danilo.silva@msn.com>
2022-11-20 22:30:13 -0300

    Assert com Eventos

---
+++ Testing_Code_XUnit_Dotnet_Getting_Started/11_Event_Asserts/Game/GameEngine.Tests/BossShould.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class BossShould
+    {
+        [Fact]
+        [Trait("Categoria", "Boss")]
+        public void HaveCorrectPower()
+        {
+            Boss sut = new Boss();
+
+            Assert.Equal(166.667, sut.AtaquePoderEspecialTotal, 3);
+        }
+    }
+}
---
+++ Testing_Code_XUnit_Dotnet_Getting_Started/11_Event_Asserts/Game/GameEngine/CreateInimigoException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameEngine
+{
+    public class CreateInimigoException : Exception
+    {
+        public CreateInimigoException(string mensagem, string nomeInimigo) : base(mensagem)
+        {
+            RequestedNomeInimigo = nomeInimigo;
+        }
+
+        public string RequestedNomeInimigo { get; private set; }
+    }
+}
