caa1ada83fe817b44aee98c8acd1a77b5196dadb
Danilo Silva <danilo.silva@msn.com>
2022-11-20 23:40:44 -0300

    Refatorando com Testes Data-driven

---
+++ Testing_Code_XUnit_Dotnet_Getting_Started/19_Inline_Data/Game/GameEngine.Tests/GameStateShould.cs
@@ -0,0 +1,53 @@
+﻿using Xunit.Abstractions;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class GameStateShould : IClassFixture<GameStateFixture>
+    {
+        private readonly GameStateFixture gameStateFixture;
+        private readonly ITestOutputHelper output;
+
+        public GameStateShould(GameStateFixture gameStateFixture, ITestOutputHelper output)
+        {
+            this.gameStateFixture = gameStateFixture;
+
+            this.output = output;
+        }
+
+        [Fact]
+        public void DamageAllPlayersWhenEarthquake()
+        {
+            output.WriteLine($"GameState ID = {gameStateFixture.State.Id}");
+
+            var jogador1 = new Jogador();
+            var jogador2 = new Jogador();
+
+            gameStateFixture.State.Jogadores.Add(jogador1);
+            gameStateFixture.State.Jogadores.Add(jogador2);
+
+            var saudeAposTerremoto = jogador1.Saude - GameState.DanoTerremoto;
+
+            gameStateFixture.State.Earthquake();
+
+            Assert.Equal(saudeAposTerremoto, jogador1.Saude);
+            Assert.Equal(saudeAposTerremoto, jogador2.Saude);
+        }
+
+        [Fact]
+        public void Reset()
+        {
+            output.WriteLine($"GameState ID = {gameStateFixture.State.Id}");
+
+            var jogador1 = new Jogador();
+            var jogador2 = new Jogador();
+
+            gameStateFixture.State.Jogadores.Add(jogador1);
+            gameStateFixture.State.Jogadores.Add(jogador2);
+
+            gameStateFixture.State.Reset();
+
+            Assert.Empty(gameStateFixture.State.Jogadores);
+        }
+    }
+}
