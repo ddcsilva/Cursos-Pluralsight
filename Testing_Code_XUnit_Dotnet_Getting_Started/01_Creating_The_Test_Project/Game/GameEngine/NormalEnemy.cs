e8663a2904b916b383f2099ebd5ac35151795b8a
danilo.silva <danilo.silva@msn.com>
2024-03-14 22:48:19 -0300

    Reset

--- Testing_Code_XUnit_Dotnet_Getting_Started/01_Creating_The_Test_Project/Game/GameEngine/NormalEnemy.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GameEngine
-{
-    public class InimigoNormal : Inimigo
-    {
-        public override double PoderEspecialTotal => 100;
-        public override double PoderEspecialUsado => 2;
-    }
-}
--- Testing_Code_XUnit_Dotnet_Getting_Started/09_Object_Instances_Asserts/Game/GameEngine/Boss.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GameEngine
-{
-    public class Boss : Inimigo
-    {
-        public override double PoderEspecialTotal => 1000;
-        public override double PoderEspecialUsado => 6;
-    }
-}
