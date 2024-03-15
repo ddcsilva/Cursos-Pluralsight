5742c44d7f9b459fbd0cc11e5bb1acbf1a6c33b2
danilo.silva <danilo.silva@msn.com>
2024-03-15 19:55:54 -0300

    Retornando Dados

---
+++ ASP-NET-Core-6-Web-API/ASP-NET-Core-Web-API-Fundamentals/CityInfo/CityInfo.API/Controllers/CidadesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers;
+
+[ApiController]
+[Route("api/cidades")]
+public class CidadesController : ControllerBase
+{
+    [HttpGet]
+    public JsonResult ObterCidades()
+    {
+        return new JsonResult(new List<object>
+        {
+            new { Id = 1, Nome = "New York City" },
+            new { Id = 2, Nome = "Antwerp" }
+        });
+    }
+}
