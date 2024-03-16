3f159683865f0984f69957c576612d3bf76c42dd
danilo.silva <danilo.silva@msn.com>
2024-03-15 22:53:19 -0300

    Manipulando a Resposta de Erro Padrão

--- ASP-NET-Core-6-Web-API/ASP-NET-Core-Web-API-Fundamentals/CityInfo/CityInfo.API/Program.cs
+++ ASP-NET-Core-6-Web-API/ASP-NET-Core-Web-API-Fundamentals/CityInfo/CityInfo.API/Program.cs
@@ -3,6 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddProblemDetails(options =>
+{
+    options.CustomizeProblemDetails = ctx =>
+    {
+        ctx.ProblemDetails.Extensions.Add("server", Environment.MachineName);
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,9 +31,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
+app.MapControllers();
 
 app.Run();
