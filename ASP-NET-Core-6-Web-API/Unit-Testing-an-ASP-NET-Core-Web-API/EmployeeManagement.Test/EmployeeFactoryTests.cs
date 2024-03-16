f4bb1448e29ca4f1203d3d4d6569e666c9f9af38
danilo.silva <danilo.silva@msn.com>
2024-03-16 16:04:08 -0300

    Escrevendo Primeiro Teste Unitário

---
+++ ASP-NET-Core-6-Web-API/Unit-Testing-an-ASP-NET-Core-Web-API/EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test;
+
+public class EmployeeFactoryTests
+{
+    [Fact]
+    public void CriarFuncionario_ConstrutorInternoFuncionario_SalarioDeveSer2500()
+    {
+        var funcionarioFactory = new EmployeeFactory();
+        var funcionario = (InternalEmployee)funcionarioFactory.CreateEmployee("Danilo", "Silva");
+
+        Assert.Equal(2500, funcionario.Salary);
+    }
+}
