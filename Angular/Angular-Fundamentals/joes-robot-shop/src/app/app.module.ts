35c6198d6a4a687d41ed2875fffe911541029f73
danilo.silva <danilo.silva@msn.com>
2024-03-30 15:22:03 -0300

    Trabalhando com Bindings

--- Angular/Angular-Fundamentals/joes-robot-shop/src/app/app.module.ts
+++ Angular/Angular-Fundamentals/joes-robot-shop/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
-import { SiteHeaderComponent } from '_course-resources/site-header/site-header.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @NgModule({
   declarations: [
--- Angular/Angular-Fundamentals/joes-robot-shop/src/app/catalog/catalog.component.ts
+++ Angular/Angular-Fundamentals/joes-robot-shop/src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.model';
 
 @Component({
   selector: 'app-catalog',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
+  public product: IProduct;
 
+  constructor() {
+    this.product = {
+      id: 2,
+      description: 'A friendly robot with two eyes and a smile -- great for domestic use.',
+      name: 'Friendly Bot',
+      imageName: 'head-friendly.png',
+      category: 'Heads',
+      price: 945.0,
+      discount: 0.2
+    };
+  }
+
+  public obterCaminhoImagem(product: IProduct): string {
+    return '/assets/images/robot-parts/' + product.imageName;
+  }
 }
