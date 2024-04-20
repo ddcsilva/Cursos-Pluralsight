bc03b0937427f1ae7a5037d4c0f371b4fbeb2dd5
danilo.silva <danilo.silva@msn.com>
2024-04-20 00:00:19 -0300

    Criação do Controller

---
+++ ASP-NET-Core-6/ASP-NET-Core-Fundamentals/BethanysPieShop/BethanysPieShop/Controllers/TortasController.cs
@@ -0,0 +1,22 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers;
+
+public class TortasController : Controller
+{
+    private readonly ITortaRepository _tortaRepository;
+    private readonly ICategoriaRepository _categoriaRepository;
+
+    public TortasController(ITortaRepository tortaRepository, ICategoriaRepository categoriaRepository)
+    {
+        _tortaRepository = tortaRepository;
+        _categoriaRepository = categoriaRepository;
+    }
+
+    public IActionResult Listar()
+    {
+        var tortas = _tortaRepository.ObterTortas;
+        return View(tortas);
+    }
+}
