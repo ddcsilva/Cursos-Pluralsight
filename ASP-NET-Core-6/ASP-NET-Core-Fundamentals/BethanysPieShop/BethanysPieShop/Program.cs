eea3df2543e11a9fc3fa29e1ecfef90a5fdf9f0d
danilo.silva <danilo.silva@msn.com>
2024-04-19 23:35:57 -0300

    Configurando a Aplicação

--- ASP-NET-Core-6/ASP-NET-Core-Fundamentals/BethanysPieShop/BethanysPieShop/Program.cs
+++ ASP-NET-Core-6/ASP-NET-Core-Fundamentals/BethanysPieShop/BethanysPieShop/Program.cs
@@ -1,6 +1,24 @@
+// WebApplication.CreateBuilder: é um método de extensão que cria um WebApplicationBuilder, responsável por configurar a aplicação web
 var builder = WebApplication.CreateBuilder(args);
+
+// AddControllerWithViews: é um método de extensão que adiciona suporte para controllers e views
+builder.Services.AddControllersWithViews();
+
+// Build: é um método que cria uma instância de WebApplication, responsável por gerenciar a aplicação web
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// UseStaticFiles: é um método de extensão que adiciona suporte para arquivos estáticos (Pasta: wwwroot)
+app.UseStaticFiles();
 
+// IsDevelopment: é uma propriedade que verifica se a aplicação está em ambiente de desenvolvimento
+if (app.Environment.IsDevelopment())
+{
+    // UseDeveloperExceptionPage: é um método de extensão que adiciona suporte para páginas de erro de desenvolvedor
+    app.UseDeveloperExceptionPage();
+}
+
+// MapDefaultControllerRoute: é um método de extensão que mapeia a rota padrão para controllers
+app.MapDefaultControllerRoute();
+
+// Run: é um método que inicia a aplicação web
 app.Run();
