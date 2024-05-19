23ee864311691d12828737e2e9e3d8583db81d20
danilo.silva <danilo.silva@msn.com>
2024-05-19 00:27:38 -0300

    feat: Add site header and catalog component

--- Angular/Angular-Fundamentals/joes-robot-shop/src/app/app.module.ts
+++ Angular/Angular-Fundamentals/joes-robot-shop/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CatalogoComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule
