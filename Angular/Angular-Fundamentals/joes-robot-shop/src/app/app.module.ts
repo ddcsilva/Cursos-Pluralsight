d84435f24108fad442f287984469a0ba4befb8c8
danilo.silva <danilo.silva@msn.com>
2024-05-18 23:37:34 -0300

    Reset

--- Angular/Angular-Fundamentals/joes-robot-shop/src/app/app.module.ts
+++ Angular/Angular-Fundamentals/joes-robot-shop/src/app/app.module.ts
@@ -2,31 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { CatalogComponent } from './catalog/catalog.component';
-import { SiteHeaderComponent } from './site-header/site-header.component';
-import { registerLocaleData } from '@angular/common';
-import localePt from '@angular/common/locales/pt';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { HttpClientModule } from '@angular/common/http';
-import { CarrinhoComponent } from './carrinho/carrinho.component';
-import { AppRoutingModule } from './app-routing.module';
-
-registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    CatalogComponent,
-    SiteHeaderComponent,
-    ProductDetailsComponent,
-    CarrinhoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
