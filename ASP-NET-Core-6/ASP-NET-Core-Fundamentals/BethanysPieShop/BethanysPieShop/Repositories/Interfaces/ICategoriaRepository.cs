79d95a817c1891a12cbc82803ae7ed924c614db4
danilo.silva <danilo.silva@msn.com>
2024-05-26 20:18:43 -0300

    Reset

--- ASP-NET-Core-6/ASP-NET-Core-Fundamentals/BethanysPieShop/BethanysPieShop/Repositories/Interfaces/ICategoriaRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using BethanysPieShop.Models;
-
-namespace BethanysPieShop.Repositories.Interfaces;
-
-public interface ICategoriaRepository
-{
-    IEnumerable<Categoria> ObterCategorias { get; }
-}
