1ff2d477909d681ee5f01f6cfe278ff0effd3c6f
danilo.silva <danilo.silva@msn.com>
2024-06-23 17:50:39 -0300

    Usando View Model

--- ASP.NET Core Fundamentals/BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
+++ ASP.NET Core Fundamentals/BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -1,16 +1,23 @@
 ﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers;
 
-public class PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository) : Controller
+public class PieController : Controller
 {
-    private readonly IPieRepository _pieRepository = pieRepository;
-    private readonly ICategoryRepository _categoryRepository = categoryRepository;
+    private readonly IPieRepository _pieRepository;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+    {
+        _pieRepository = pieRepository;
+        _categoryRepository = categoryRepository;
+    }
 
     public IActionResult List()
     {
-        ViewBag.CurrentCategory = "Cheese cakes";
-        return View(_pieRepository.AllPies);
+        PieListViewModel pieListViewModel = new(_pieRepository.AllPies, "All pies");
+        return View(pieListViewModel);
     }
 }
