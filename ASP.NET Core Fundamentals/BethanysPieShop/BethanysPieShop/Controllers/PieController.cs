9010d0d20baed8d0c617ab360b68f6e3a0f93767
danilo.silva <danilo.silva@msn.com>
2024-06-23 01:04:02 -0300

    Adicionando o Controller

---
+++ ASP.NET Core Fundamentals/BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,15 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers;
+
+public class PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository) : Controller
+{
+    private readonly IPieRepository _pieRepository = pieRepository;
+    private readonly ICategoryRepository _categoryRepository = categoryRepository;
+
+    public IActionResult List()
+    {
+        return View(_pieRepository.AllPies);
+    }
+}
