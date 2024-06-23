29904092a6910965bb6efb8e7fdac67fab6652f3
danilo.silva <danilo.silva@msn.com>
2024-06-23 19:12:50 -0300

    Criando o Repositório

---
+++ ASP.NET Core Fundamentals/BethanysPieShop/BethanysPieShop/Models/PieRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Models;
+
+public class PieRepository : IPieRepository
+{
+    private readonly BethanysPieShopDbContext _context;
+
+    public PieRepository(BethanysPieShopDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Pie> AllPies => _context.Pies.Include(c => c.Category);
+
+    public IEnumerable<Pie> PiesOfTheWeek => _context.Pies.Include(c => c.Category).Where(p => p.IsPieOfTheWeek);
+
+    public Pie? GetPieById(int pieId) => _context.Pies.FirstOrDefault(p => p.PieId == pieId);
+}
