eb82ee01d317534aaaa69bf124dd0c550dc4677d
danilo.silva <danilo.silva@msn.com>
2024-06-30 20:57:37 -0300

    Demonstração: Retornando Recursos (Parte 1)

---
+++ ASP.NET Core Web API Fundamentals/CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers;
+
+[ApiController]
+public class CitiesController : ControllerBase
+{
+    public JsonResult GetCities()
+    {
+        return new JsonResult(new List<object>
+        {
+            new { Id = 1, Name = "New York City" },
+            new { Id = 2, Name = "Antwerp" }
+        });
+    }
+}
