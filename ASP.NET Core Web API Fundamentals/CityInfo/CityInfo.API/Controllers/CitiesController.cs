14c0a762e0e1b82851a71021761c5d7c2ea553fb
danilo.silva <danilo.silva@msn.com>
2024-06-30 21:30:04 -0300

    Aprimorando a Arquitetura com Classes de Modelo

--- ASP.NET Core Web API Fundamentals/CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ ASP.NET Core Web API Fundamentals/CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers;
 
@@ -9,10 +10,19 @@ public class CitiesController : ControllerBase
     [HttpGet]
     public JsonResult GetCities()
     {
-        return new JsonResult(new List<object>
+        return new JsonResult(CitiesDataStore.Current.Cities);
+    }
+
+    [HttpGet("{id}")]
+    public JsonResult GetCity(int id)
+    {
+        var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+        if (cityToReturn == null)
         {
-            new { Id = 1, Name = "New York City" },
-            new { Id = 2, Name = "Antwerp" }
-        });
+            return new JsonResult("City not found") { StatusCode = 404 };
+        }
+
+        return new JsonResult(cityToReturn);
     }
 }
---
+++ ASP.NET Core Web API Fundamentals/CityInfo/CityInfo.API/Models/CitiesDataStore.cs
@@ -0,0 +1,32 @@
+﻿namespace CityInfo.API.Models;
+
+public class CitiesDataStore
+{
+    public static CitiesDataStore Current { get; } = new CitiesDataStore();
+    public List<CityDTO> Cities { get; set; }
+
+    public CitiesDataStore()
+    {
+        Cities =
+        [
+            new CityDTO
+            {
+                Id = 1,
+                Name = "New York City",
+                Description = "The one with that big park."
+            },
+            new CityDTO
+            {
+                Id = 2,
+                Name = "Antwerp",
+                Description = "The one with the cathedral that was never really finished."
+            },
+            new CityDTO
+            {
+                Id = 3,
+                Name = "Paris",
+                Description = "The one with that big tower."
+            }
+        ];
+    }
+}
