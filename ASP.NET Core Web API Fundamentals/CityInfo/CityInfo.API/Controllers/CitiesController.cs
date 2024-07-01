9da0a9ff8d18681aabb4050563848222dd939076
danilo.silva <danilo.silva@msn.com>
2024-06-30 21:40:45 -0300

    Retornando Códigos de Status Corretos

--- ASP.NET Core Web API Fundamentals/CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ ASP.NET Core Web API Fundamentals/CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -8,21 +8,21 @@ namespace CityInfo.API.Controllers;
 public class CitiesController : ControllerBase
 {
     [HttpGet]
-    public JsonResult GetCities()
+    public ActionResult<IEnumerable<CityDTO>> GetCities()
     {
-        return new JsonResult(CitiesDataStore.Current.Cities);
+        return Ok(CitiesDataStore.Current.Cities);
     }
 
     [HttpGet("{id}")]
-    public JsonResult GetCity(int id)
+    public ActionResult<CityDTO> GetCity(int id)
     {
         var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
 
         if (cityToReturn == null)
         {
-            return new JsonResult("City not found") { StatusCode = 404 };
+            return NotFound();
         }
 
-        return new JsonResult(cityToReturn);
+        return Ok(cityToReturn);
     }
 }
